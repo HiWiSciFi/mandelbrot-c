3dee149ca5445cc40fa714f2ba7d9cd60a0d2a5b
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-04-29 17:09:31 +0200

    Removed logs from main

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 #include "imageprocessing.h"
 
 int main() {
-    printf("1\n");
     struct color* img = createImage(imageSize.x, imageSize.y);
-    printf("2\n");
     saveImage(img, "output.png");
     struct color c = { 255, 0, 0 };
     for (int x = 0; x < 100; x++) {
@@ -14,8 +12,6 @@ int main() {
             setPixel(img, x, y, c);
         }
     }
-    printf("3\n");
     freeImage(img);
-    printf("4\n");
     return 0;
 }
