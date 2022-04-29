166c650ee0f9c35ffecc876f7c513f7c81e15589
imurnaili <54451121+imurnaili@users.noreply.github.com>
2022-04-29 19:18:26 +0200

    added color modes

--- color.h
+++ color.h
@@ -3,88 +3,101 @@
 #include "types.h"
 #include "config.h"
 
-struct color getColor(int value){
-    if (value == -1){
-        return centerColor;
+struct color hsvToRgb(float h, float s, float v);
 
+struct color getColor(int value){
+    if (value == -1) {
+        return centerColor;
     }
-    float r = ((float)(ITERATIONS - value)) / ITERATIONS;
-    struct color result;
-    if(r > 0.5f){
-        r *= 2;
-        result.r = (color2.r + (color1.r - color2.r) * r);
-        result.g = (color2.g + (color1.g - color2.g) * r);
-        result.b = (color2.b + (color1.b - color2.b) * r);
-    } else {
-        r = (r - 0.5f) * 2;
-        result.r = (color2.r + (color2.r - color3.r) * r);
-        result.g = (color2.g + (color2.g - color3.g) * r);
-        result.b = (color2.b + (color2.b - color3.b) * r);
+    if (COLOR_MODE == 0 || COLOR_MODE > 2) {
+        float r = ((float) (ITERATIONS - value)) / ITERATIONS;
+        struct color result;
+        if (r > 0.5f) {
+            r *= 2;
+            result.r = (color2.r + (color1.r - color2.r) * r);
+            result.g = (color2.g + (color1.g - color2.g) * r);
+            result.b = (color2.b + (color1.b - color2.b) * r);
+        } else {
+            r = (r - 0.5f) * 2;
+            result.r = (color2.r + (color2.r - color3.r) * r);
+            result.g = (color2.g + (color2.g - color3.g) * r);
+            result.b = (color2.b + (color2.b - color3.b) * r);
+        }
+        return result;
+    } else if (COLOR_MODE == 1){
+        float h = (float)(value % REPEAT_AFTER) / REPEAT_AFTER * 360.0f;
+        return hsvToRgb(h, HSV_S, HSV_V);
+    }else if (COLOR_MODE == 2){
+        if (value == 0) {
+            struct color black = {0,0,0};
+            return black;
+        }
+        int index = value % sizeof (colorArray) / sizeof (struct color);
+        return colorArray[index];
     }
-    return result;
 }
 
-struct color hsvToRgb(float H, float S, float V) {
+struct color hsvToRgb(float h, float s, float v) {
 
     double r = 0, g = 0, b = 0;
 
-    if (S == 0)
+    if (s == 0)
     {
-        r = V;
-        g = V;
-        b = V;
+        g = v;
+        b = v;
+        r = v;
     }
     else
     {
         int i;
         double f, p, q, t;
 
-        if (H == 360)
-            H = 0;
+        if (h == 360)
+            h = 0;
         else
-            H = H / 60;
+            h = h / 60;
 
-        i = (int)trunc(H);
-        f = H - i;
+        i = (int)trunc(h);
+        f = h - i;
 
-        p = V * (1.0 - S);
-        q = V * (1.0 - (S * f));
-        t = V * (1.0 - (S * (1.0 - f)));
+        p = v * (1.0 - s);
+        q = v * (1.0 - (s * f));
+        t = v * (1.0 - (s * (1.0 - f)));
 
         switch (i)
         {
             case 0:
-                r = V;
+                r = v;
                 g = t;
                 b = p;
                 break;
 
             case 1:
                 r = q;
-                g = V;
+                g = v;
                 b = p;
                 break;
 
             case 2:
                 r = p;
-                g = V;
+                g = v;
                 b = t;
                 break;
 
             case 3:
                 r = p;
                 g = q;
-                b = V;
+                b = v;
                 break;
 
             case 4:
                 r = t;
                 g = p;
-                b = V;
+                b = v;
                 break;
 
             default:
-                r = V;
+                r = v;
                 g = p;
                 b = q;
                 break;
--- config.h
+++ config.h
@@ -5,9 +5,43 @@
 ////////////////////////////////////////////////////////////
 
 #define CENTER_COLOR { 0, 0, 0 }
+
+#define COLOR_MODE 2
+////
+// Mode 0 - Gradient
+///
 #define COLOR_1 { 0, 0, 0 }
 #define COLOR_2 { 0, 255, 0 }
 #define COLOR_3 { 255, 255, 255 }
+
+////
+// Mode 1 - HSV lsd
+///
+#define REPEAT_AFTER 10
+#define HSV_S 1
+#define HSV_V 1
+
+////
+// Mode 2 - array mode
+///
+#define COLOR_ARRAY { \
+    {66, 30, 15},\
+    {25, 7, 26},\
+    {9, 1, 47},\
+    {4, 4, 73},\
+    {0, 7, 100},\
+    {12, 44, 138},\
+    {24, 82, 177},\
+    {57, 125, 209},\
+    {134, 181, 229},\
+    {211, 236, 248},\
+    {241, 233, 191},\
+    {248, 201, 95},\
+    {255, 170, 0},\
+    {204, 128, 0},\
+    {153, 87, 0},\
+    {106, 52, 3}\
+}
 
 #define ITERATIONS 30
 #define IMAGE_SIZE { 2000, 2000 }
@@ -27,6 +61,8 @@ struct color centerColor = CENTER_COLOR;
 struct color color1 = COLOR_1;
 struct color color2 = COLOR_2;
 struct color color3 = COLOR_3;
+
+struct color colorArray[] = COLOR_ARRAY;
 
 struct vec2i imageSize = IMAGE_SIZE;
 
