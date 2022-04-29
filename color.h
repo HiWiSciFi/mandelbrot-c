d74a441090710bb92ec6b048c6542012da3397e5
imurnaili <54451121+imurnaili@users.noreply.github.com>
2022-04-29 17:55:21 +0200

    color bugfix

--- color.h
+++ color.h
@@ -11,19 +11,16 @@ struct color getColor(int value){
     }
     float r = ((float)(ITERATIONS - value)) / ITERATIONS;
     struct color result;
-    if(r >= 0.5f){
+    if(r > 0.5f){
         r *= 2;
         result.r = (color2.r + (color1.r - color2.r) * r);
         result.g = (color2.g + (color1.g - color2.g) * r);
         result.b = (color2.b + (color1.b - color2.b) * r);
     } else {
         r = (r - 0.5f) * 2;
-        result.r = (color3.r + (color2.r - color3.r) * r);
-        result.g = (color3.g + (color2.g - color3.g) * r);
-        result.b = (color3.b + (color2.b - color3.b) * r);
-    }
-    if(result.b == 0){
-        printf("%i ", value);
+        result.r = (color2.r + (color2.r - color3.r) * r);
+        result.g = (color2.g + (color2.g - color3.g) * r);
+        result.b = (color2.b + (color2.b - color3.b) * r);
     }
     return result;
 }
