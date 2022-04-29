c32987bbfe276d5a3284e22265bb521fdf1db5f5
HiWiSciFi <19999080+HiWiSciFi@users.noreply.github.com>
2022-04-29 17:09:05 +0200

    No Errors!

--- main.c
+++ main.c
@@ -5,14 +5,15 @@
 
 int main() {
     printf("1\n");
-    struct color* img = createImage(100, 100);
+    struct color* img = createImage(imageSize.x, imageSize.y);
     printf("2\n");
     saveImage(img, "output.png");
-    /*for (int x = 0; x < 100; x++) {
+    struct color c = { 255, 0, 0 };
+    for (int x = 0; x < 100; x++) {
         for (int y = 0; y < 100; y++) {
-            setPixel(img, x, y, centerColor);
+            setPixel(img, x, y, c);
         }
-    }*/
+    }
     printf("3\n");
     freeImage(img);
     printf("4\n");
